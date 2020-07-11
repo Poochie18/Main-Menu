82f015abc90822513fee0af95553f6a8ccac56e5
Poochie18 <39067344+Poochie18@users.noreply.github.com>
2020-07-11 21:38:03 +0300

    Task 1: making simple start button and exit button

---
+++ MainMenu/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void StartPressed()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitPressed()
+    {
+        Debug.Log("Exit");
+        //Application.Quit();
+    }
+}
